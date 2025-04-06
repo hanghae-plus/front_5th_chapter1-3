361cec77455d449743ede5ca0007c7e17a0af02e
HoJae Lee <hojae.lee@softly.ai>
2025-04-06 23:56:14 +0900

    feat: useMemo

--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,12 +1,23 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { DependencyList } from "react";
 import { shallowEquals } from "../equalities";
+import { useRef } from "./useRef";
 
 export function useMemo<T>(
   factory: () => T,
   _deps: DependencyList,
   _equals = shallowEquals,
 ): T {
-  // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+  const memoizedRef = useRef<{ value: T; _deps: DependencyList } | null>(null);
+
+  if (
+    memoizedRef.current === null ||
+    !_equals(memoizedRef.current._deps, _deps)
+  ) {
+    memoizedRef.current = {
+      value: factory(),
+      _deps,
+    };
+  }
+
+  return memoizedRef.current.value;
 }
