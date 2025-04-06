bc40715e4b21e43639529b78f96acafd87dbf417
KumJungMin <rmawjdals@naver.com>
2025-04-06 21:34:57 +0900

    refactor: NotificationSystem 분리하기

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import { Header } from "./Header";
 import { ItemList } from "./ItemList";
 import { ComplexForm } from "./ComplexForm";
+import { NotificationSystem } from "./NotificationSystem";
 
-export { Header, ItemList, ComplexForm };
+export { Header, ItemList, ComplexForm, NotificationSystem };
