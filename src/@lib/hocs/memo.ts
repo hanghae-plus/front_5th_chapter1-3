9b4cd7f18ab7b505cd0c899f30e1463a925a824e
HoJae Lee <hojae.lee@softly.ai>
2025-04-07 00:33:20 +0900

    feat: memo

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,24 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
 import { ComponentType } from "react";
+import { useRef } from "../hooks";
+import React from "react";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals,
 ) {
-  return Component;
+  return function MemoizedComponent(props: P) {
+    const prevPropsRef = useRef<P | null>(null);
+    const renderedElementRef = useRef<React.ReactElement | null>(null);
+
+    const shouldUpdate =
+      !prevPropsRef.current || !_equals(prevPropsRef.current, props);
+
+    if (shouldUpdate) {
+      prevPropsRef.current = props;
+      renderedElementRef.current = React.createElement(Component, props);
+    }
+
+    return renderedElementRef.current;
+  };
 }
