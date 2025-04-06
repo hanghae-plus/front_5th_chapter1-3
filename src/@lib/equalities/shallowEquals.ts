4fd7e6c6c6680cecb9fe2d73548fbed5601b2822
KumJungMin <rmawjdals@naver.com>
2025-04-06 18:53:38 +0900

    feat: shallowEquals 구현하기

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,40 @@
-export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+export function shallowEquals<T>(a: T, b: T): boolean {
+  if (a === b) return true;
+
+  if (Array.isArray(a) && Array.isArray(b)) {
+    return arrayShallowEquals(a, b);
+  }
+
+  if (isObject(a) && isObject(b)) {
+    return objectShallowEquals(a, b);
+  }
+
+  return false;
+}
+
+function arrayShallowEquals<T>(arrA: T[], arrB: T[]): boolean {
+  if (arrA.length !== arrB.length) return false;
+  for (let i = 0; i < arrA.length; i++) {
+    if (arrA[i] !== arrB[i]) return false;
+  }
+  return true;
+}
+
+function isObject(item: unknown): item is Record<string, unknown> {
+  return typeof item === "object" && item !== null && !Array.isArray(item);
+}
+
+function objectShallowEquals(
+  objA: Record<string, unknown>,
+  objB: Record<string, unknown>,
+): boolean {
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+
+  if (keysA.length !== keysB.length) return false;
+
+  for (const key of keysA) {
+    if (objA[key] !== objB[key]) return false;
+  }
+  return true;
 }
