ce9f440e4f9ec97717ceecbb62dbc6a6d48e445b
HoJae Lee <hojae.lee@softly.ai>
2025-04-06 23:55:47 +0900

    feat: shallowEquals

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,29 @@
+import { isRecord } from "../utils/isRecord";
+
 export function shallowEquals<T>(objA: T, objB: T): boolean {
+  if (!objA || !objB) return objA === objB;
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) return false;
+
+    return objA.every((item, index) => item === objB[index]);
+  }
+
+  if (isRecord(objA) && isRecord(objB)) {
+    return shallowEqualsRecord(objA, objB);
+  }
+
   return objA === objB;
+}
+
+function shallowEqualsRecord(
+  a: Record<string, unknown>,
+  b: Record<string, unknown>,
+) {
+  const aKeys = Object.keys(a);
+  const bKeys = Object.keys(b);
+
+  if (aKeys.length !== bKeys.length) return false;
+
+  return aKeys.every((key) => a[key] === b[key]);
 }
