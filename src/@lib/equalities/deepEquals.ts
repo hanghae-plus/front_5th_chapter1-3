84a87600996fd37471177945d77546e879300284
minhee <jmh3699@naver.com>
2025-04-07 20:01:23 +0900

    feat: deepEquals 깊은 비교 구현
    - 배열의 경우 길이가 같다면 배열을 순회하며 재귀적으로 배열의 요소를 비교한다.
    - 객체일 경우 재귀적으로 속성을 비교한다.

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,37 @@
+import { isNull, isPrimitive, isObject } from "../typeValidation";
+
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (Object.is(objA, objB)) {
+    return true;
+  }
+
+  if (isNull(objA) || isNull(objB)) {
+    return false;
+  }
+
+  if (isPrimitive(objA) || isPrimitive(objB)) {
+    return false;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+
+    return objA.every((value: unknown, key: number) =>
+      deepEquals(value, objB[key]),
+    );
+  }
+
+  // 객체 비교
+  if (!isObject(objA) || !isObject(objB)) {
+    return false;
+  }
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  return keysA.every((key) => deepEquals(objA[key], objB[key]));
 }
