8aa79e20db0152ba700d8d8201dccdf2a1e71ed5
3jun <kk3june@gmail.com>
2025-04-07 14:21:59 +0900

    feat. 비교함수 구현

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,21 @@
+import { isObject } from "../../utils";
+
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) return true;
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) return false;
+
+    return objA.every((value, key) => deepEquals(objB[key], value));
+  }
+
+  if (isObject(objA) && isObject(objB)) {
+    const entriesA = Object.entries(objA);
+    const entriesB = Object.entries(objB);
+
+    if (entriesA.length !== entriesB.length) return false;
+    return entriesA.every(([key, value]) => deepEquals(objB[key], value));
+  }
+
+  return false;
 }
--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,21 @@
+import { isObject } from "../../utils";
+
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) return true;
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) return false;
+
+    return objA.every((value, key) => objB[key] === value);
+  }
+
+  if (isObject(objA) && isObject(objB)) {
+    const entriesA = Object.entries(objA);
+    const entriesB = Object.entries(objB);
+
+    if (entriesA.length !== entriesB.length) return false;
+    return entriesA.every(([key, value]) => objB[key] === value);
+  }
+
+  return false;
 }
