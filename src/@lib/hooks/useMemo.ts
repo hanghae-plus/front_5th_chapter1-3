0df0f875abef34ca19cae6b73ff4861f8484ca98
3jun <kk3june@gmail.com>
2025-04-07 14:57:46 +0900

    feat. react hook 구현

--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,6 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { DependencyList } from "react";
 import { shallowEquals } from "../equalities";
+import { useRef } from "./useRef";
 
 export function useMemo<T>(
   factory: () => T,
@@ -8,5 +8,22 @@ export function useMemo<T>(
   _equals = shallowEquals,
 ): T {
   // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+  /**
+   * - 의존성의 값들이 변경될 때만 재계산
+   * - 매개변수 : calculateValue, dependencies
+   * - 초기 랜더링 시에는 인자 없이 calculateValue를 호출한 값 반환
+   */
+  const ref = useRef({
+    value: undefined as T,
+    deps: [] as DependencyList,
+    initialize: false,
+  });
+
+  if (!ref.current || !_equals(ref.current.deps, _deps)) {
+    ref.current.value = factory();
+    ref.current.deps = _deps;
+    ref.current.initialize = true;
+  }
+
+  return ref.current.value;
 }
