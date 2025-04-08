bf5b0d67e0595e621df3889ca01933cf38d78a3f
minhee <jmh3699@naver.com>
2025-04-08 19:43:13 +0900

    refactor: 프로젝트 구조 개선
    - types 디렉토리로 interface 이동
    - components 디렉토리로 Header, ItemList, ComplexForm, NotificationSystem 컴포넌트 이동
    - useContext로 context api 관련 interface, api 이동

---
+++ src/context/useContext.ts
@@ -0,0 +1,25 @@
+import { createContext, useContext } from "react";
+
+import { Notification } from "../types/Notification";
+import { User } from "../types/User";
+
+export interface AppContextType {
+  theme: string;
+  toggleTheme: () => void;
+  user: User | null;
+  login: (email: string, password: string) => void;
+  logout: () => void;
+  notifications: Notification[];
+  addNotification: (message: string, type: Notification["type"]) => void;
+  removeNotification: (id: number) => void;
+}
+
+export const AppContext = createContext<AppContextType | undefined>(undefined);
+
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  if (context === undefined) {
+    throw new Error("useAppContext must be used within an AppProvider");
+  }
+  return context;
+};
