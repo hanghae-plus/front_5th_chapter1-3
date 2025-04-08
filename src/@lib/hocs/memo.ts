2b572943f055519ebd9b687a201b02423f3b1804
minhee <jmh3699@naver.com>
2025-04-08 12:36:47 +0900

    feat: memo 함수 구현
    - 이전 props와 현재 props를 비교해 다를 경우에만 새로 렌더링하는 방식으로 컴포넌트를 메모이제이션한다.

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,24 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { ComponentType, createElement } from "react";
+import { useRef } from '../hooks';
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals,
 ) {
-  return Component;
+  const MemoizedComponent = (props: P) => {
+    const prevRef = useRef<P | null>(null);
+    const renderRef = useRef<React.ReactElement | null>(null);
+
+    // 현재 props와 이전 props를 비교하여 다르면 렌더링
+    if (!prevRef.current || !_equals(prevRef.current, props)) {
+      prevRef.current = props;
+      renderRef.current = createElement(Component, props);
+    }
+
+    return renderRef.current;
+  };
+
+  return MemoizedComponent;
 }
