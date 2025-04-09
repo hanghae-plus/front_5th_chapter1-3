4c9ab0304647de655806e6253d42f3df8632b8e9
KumJungMin <rmawjdals@naver.com>
2025-04-09 22:22:57 +0900

    remove: 미사용 파일 제거하기

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import { Header } from "./Header";
-import { ItemList } from "./ItemList";
-import { ComplexForm } from "./ComplexForm";
-import { NotificationSystem } from "./NotificationSystem";
-
-export { Header, ItemList, ComplexForm, NotificationSystem };
